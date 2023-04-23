1091d0ab631265bf2dd44276ac3474278ae2c262
Dmitry Mishanov <d.mishanov@cian.ru>
2023-04-23 15:01:58 +0300

    arrange-act-assert

--- tests/test_case.py
+++ tests/test_case.py
@@ -2,23 +2,31 @@ from src.case import Case, CaseStatus
 
 
 def test_case__ok():
-
+    # arrange
     def my_owesome_test_case():
         assert 2 + 2 == 4
 
     case = Case(my_owesome_test_case)
+
+    # act
     case.run()
+
+    # assert
     assert case.ran
     assert case.status is CaseStatus.success
 
 
 def test_case__failed():
-
+    # arrange
     def my_owesome_test_case():
         assert 2 + 2 == 5
 
     case = Case(my_owesome_test_case)
+
+    # act
     case.run()
+
+    # assert
     assert case.ran
     assert case.status is CaseStatus.failed
     assert case.error == 'assert (2 + 2) == 5'
